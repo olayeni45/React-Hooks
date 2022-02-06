590e5a79811499fec19511b45684c0b8af9f2469
Olayeni Anifowose <olayeni45@gmail.com>
2022-02-06 15:01:31 +0000

    useEffect cleanup Function

--- react-hooks/src/components/Login/Login.jsx
+++ react-hooks/src/components/Login/Login.jsx
@@ -7,7 +7,7 @@ const Login = (props) => {
   const { onLogin } = props;
 
   const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();      
+  const [emailIsValid, setEmailIsValid] = useState();
 
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
@@ -15,9 +15,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log("checking form validity");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log("---CLEANUP---");
+      clearTimeout(identifier)
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
