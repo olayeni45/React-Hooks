28c3f6c9aeb65f492ba6ad612b16097a449b82f6
Olayeni Anifowose <olayeni45@gmail.com>
2022-02-28 13:00:59 +0000

    starting files

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,9 +1,12 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import "./App.css";
 import Button from "./components/UI/Button/Button";
-import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
 
 const App = () => {
+  const [listTitle, setListTitle] = useState("My List");
+
+  /*
   const [showParagraph, setShowParagraph] = useState(false);
 
   const [allowToggle, setAllowToggle] = useState(false);
@@ -18,15 +21,21 @@ const App = () => {
     setAllowToggle(true);
   };
 
+  */
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+ // const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
   console.log("APP RUNNING");
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
-
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+      <DemoList title={listTitle} items={[100, 5, 3, 1, 10, 9]} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 };
---
+++ react-hooks/src/components/Demo/DemoList.jsx
@@ -0,0 +1,28 @@
+import React, { useMemo } from "react";
+
+import styles from "./DemoList.module.css";
+
+const DemoList = (props) => {
+  const { title, items } = props;
+
+  const sortedList = items.sort((a, b) => a - b);
+
+  //   const sortedList = useMemo(() => {
+  //     console.log("Items sorted");
+  //     return items.sort((a, b) => a - b);
+  //   }, [items]);
+  console.log("DemoList RUNNING");
+
+  return (
+    <div className={styles.list}>
+      <h2>{title}</h2>
+      <ul>
+        {sortedList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default React.memo(DemoList);
