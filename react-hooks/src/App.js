1e0d10c537c9e72cddd3000fcbecafc0482bd719
Olayeni Anifowose <olayeni45@gmail.com>
2022-03-09 20:45:54 +0000

    custom hooks - starting files

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,29 +1,14 @@
-import React, { Fragment, useState } from "react";
-import MainHeader from "./components/MainHeader/MainHeader";
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
+import React, {Fragment} from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const LoginHandler = (email, password) => {
-    console.log({ email, password });
-    setIsLoggedIn(true);
-  };
-
-  const LogoutHandler = () => {
-    setIsLoggedIn(false);
-  };
-
+const App = ()=> {
   return (
     <Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={LogoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={LoginHandler} />}
-        {isLoggedIn && <Home onLogout={LogoutHandler} />}
-      </main>
+      <ForwardCounter />
+      <BackwardCounter />
     </Fragment>
   );
-};
+}
 
 export default App;
